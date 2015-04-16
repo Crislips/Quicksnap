feb108ee4f9ffeaa2489c2a833a3a06bbc185e2c
Crislips <lol-at-pokeylope@hotmail.com>
2015-04-15 21:28:28 -0400

    Two Uses of OnDrawGizmos()
    Start at Two Uses of OnDrawGizmos()

---
+++ QuickSnap/Assets/__Scripts/Shot.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable] // Make this visible in the Inspector
+public class Shot { // Shot does not extend MonoBehaviour
+	static public List<Shot> shots = new List<Shot>(); // List of all shots
+	static public string prefsName = "QuickSnap_Shots";
+	public Vector3 position; // Position of the Camera
+	public Quaternion rotation; // Rotation of the Camera
+	public Vector3 target; // Where the Camera is pointed
+
+	// Generates a single-line <shot> entry for an XML document
+	public string ToXML() {
+		string ss = "<shot ";
+		ss += "x=\""+position.x+"\" "; // 1
+		ss += "y=\""+position.y+"\" ";
+		ss += "z=\""+position.z+"\" ";
+		ss += "qx=\""+rotation.x+"\" ";
+		ss += "qy=\""+rotation.y+"\" ";
+		ss += "qz=\""+rotation.z+"\" ";
+		ss += "qw=\""+rotation.w+"\" ";
+		ss += "tx=\""+target.x+"\" ";
+		ss += "ty=\""+target.y+"\" ";
+		ss += "tz=\""+target.z+"\" ";
+		ss += " />";
+
+		return(ss);
+	}
+
+	// Takes a PT_XMLHashtable from PT_XMLReader of a <shot> entry in XML and
+	// parses it into a Shot
+	static public Shot ParseShotXML( PT_XMLHashtable xHT ) {
+		Shot sh = new Shot();
+		sh.position.x = float.Parse(xHT.att("x"));
+		sh.position.y = float.Parse(xHT.att("y"));
+		sh.position.z = float.Parse(xHT.att("z"));
+		sh.rotation.x = float.Parse(xHT.att("qx"));
+		sh.rotation.y = float.Parse(xHT.att("qy"));
+		sh.rotation.z = float.Parse(xHT.att("qz"));
+		sh.rotation.w = float.Parse(xHT.att("qw"));
+		sh.target.x = float.Parse(xHT.att("tx"));
+		sh.target.y = float.Parse(xHT.att("ty"));
+		sh.target.z = float.Parse(xHT.att("tz"));
+		return( sh );
+	}
+	// Loads all of the Shots from PlayerPrefs
+	static public void LoadShots() {
+		// Empty the shots List<Shot>
+		shots = new List<Shot>();
+		if (!PlayerPrefs.HasKey(prefsName)) {
+			// If there are no shots, return
+			return;
+		}
+		// Get the full XML and parse it
+		string shotsXML = PlayerPrefs.GetString(prefsName);
+		PT_XMLReader xmlr = new PT_XMLReader();
+		xmlr.Parse(shotsXML);
+		// Pull the PT_XMLHashList of all <shot>s
+		PT_XMLHashList hl = xmlr.xml["xml"][0]["shot"];
+		for (int i=0; i<hl.Count; i++) {
+			// Parse each <shot> in the PT_XMLHashlist into a Shot
+			PT_XMLHashtable ht = hl[i];
+			Shot sh = ParseShotXML(ht);
+			// Add it to the List<shot> shots
+			shots.Add(sh);
+		}
+	}
+	// Save List<Shot> shots to PlayerPrefs
+	static public void SaveShots() {
+		string xs = Shot.XML;
+		Utils.tr(xs); // Trace all the XML to the Console
+		// Set the PlayerPrefs
+		PlayerPrefs.SetString(prefsName, xs);
+		Utils.tr("PlayerPrefs."+prefsName+" has been set.");
+	}
+	// Convert all Shot.shots to XML
+	static public string XML {
+		get {
+			// Start an XML string
+			string xs = "<xml>\n";
+			// Add each of the Shots as a <shot> in XML
+			foreach( Shot sh in shots ) {
+				xs += sh.ToXML()+"\n";
+			}
+			// Add the closing XML tag
+			xs += "</xml>";
+			return(xs);
+		}
+	}
+}
---
+++ QuickSnap/Assets/__Scripts/TargetCamera.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TargetCamera : MonoBehaviour {
+	public bool editMode = true;
+	public GameObject fpCamera; // First-person Camera
+
+	public bool ________________;
+
+	public int shotNum;
+	public GUIText shotCounter, shotRating;
+	public GUITexture checkMark;
+	void Start() {
+		// Find the GUI components
+		GameObject go = GameObject.Find("ShotCounter");
+		shotCounter = go.GetComponent<GUIText>();
+		go = GameObject.Find("ShotRating");
+		shotRating = go.GetComponent<GUIText>();
+		go = GameObject.Find("_Check_64");
+		checkMark = go.GetComponent<GUITexture>();
+		// Hide the checkMark
+		checkMark.enabled = false;
+		// Load all the shots from PlayerPrefs
+		Shot.LoadShots();
+		// If there were shots stored in PlayerPrefs
+		if (Shot.shots.Count>0) {
+			shotNum = 0;
+			ShowShot(Shot.shots[shotNum]);
+		}
+		// Hide the cursor (Note: this doesn't work in the Unity Editor unless
+		// the Game pane is set to Maximize on Play.)
+		Screen.showCursor = false;
+	}
+
+	void Update () {
+		Shot sh;
+		// Mouse Input
+		if (Input.GetMouseButtonDown(0)) { // Left mouse button
+			sh = new Shot();
+			// Grab the position and rotation of fpCamera
+			sh.position = fpCamera.transform.position;
+			sh.rotation = fpCamera.transform.rotation;
+			// Shoot a ray from the camera and see what it hits
+			Ray ray = new Ray(sh.position, fpCamera.transform.forward);
+			RaycastHit hit;
+			if ( Physics.Raycast(ray, out hit) ) {
+				sh.target = hit.point;
+			}
+			// Position _TargetCamera with the Shot
+			ShowShot(sh);
+
+			Utils.tr( sh.ToXML() );
+
+			// Record a new shot
+			Shot.shots.Add(sh);
+			shotNum = Shot.shots.Count-1;
+
+		}
+
+		// Keyboard Input
+		// Use Q and E to cycle Shots
+		// Note: Either of these will throw an error if Shot.shots is empty.
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			shotNum--;
+			if (shotNum < 0) shotNum = Shot.shots.Count-1;
+			ShowShot(Shot.shots[shotNum]);
+		}
+		if (Input.GetKeyDown(KeyCode.E)) {
+			shotNum++;
+			if (shotNum >= Shot.shots.Count) shotNum = 0;
+			ShowShot(Shot.shots[shotNum]);
+		}
+		// If in editMode & Left Shift is held down...
+		if (editMode && Input.GetKey(KeyCode.LeftShift)) {
+			// Use Shift-S to Save
+			if (Input.GetKeyDown(KeyCode.S)) {
+				Shot.SaveShots();
+			}
+			// Use Shift-X to output XML to Console
+			if (Input.GetKeyDown(KeyCode.X)) {
+				Utils.tr(Shot.XML);
+			}
+		}
+
+		// Update the GUITexts
+		shotCounter.text = (shotNum+1).ToString()+" of "+Shot.shots.Count;
+		if (Shot.shots.Count == 0) shotCounter.text = "No shots exist";
+		// ^ Shot.shots.Count doesn't require .ToString() because it is assumed
+		// when the left side of the + operator is a string
+		shotRating.text = ""; // This line will be replaced later
+	}
+	public void ShowShot(Shot sh) {
+		// Position _TargetCamera with the Shot
+		transform.position = sh.position;
+		transform.rotation = sh.rotation;
+	}
+}
